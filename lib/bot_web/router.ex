3d3025d4bfba761d45717149c0d0d48aea9d7a75
oxbob <larslugs@gmail.com>
2023-04-23 11:36:38 +0200

    login wall dashboard + disable mail endpoints

--- lib/bot_web/router.ex
+++ lib/bot_web/router.ex
@@ -47,7 +47,7 @@ defmodule BotWeb.Router do
   end
 
   # Enable LiveDashboard and Swoosh mailbox preview in development
-  if Application.compile_env(:bot, :dev_routes) do
+  # if Application.compile_env(:bot, :dev_routes) do
     # If you want to use the LiveDashboard in production, you should put
     # it behind authentication and allow only admins to access it.
     # If your application does not have an admins-only section yet,
@@ -56,12 +56,12 @@ defmodule BotWeb.Router do
     import Phoenix.LiveDashboard.Router
 
     scope "/dev" do
-      pipe_through :browser
+      pipe_through [:browser, :require_authenticated_user]
 
       live_dashboard "/dashboard", metrics: BotWeb.Telemetry
-      forward "/mailbox", Plug.Swoosh.MailboxPreview
+      # forward "/mailbox", Plug.Swoosh.MailboxPreview
     end
-  end
+  # end
 
   ## Authentication routes
 
